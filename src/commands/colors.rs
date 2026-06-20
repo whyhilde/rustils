4a344881605a69901f9d7327d64e6fbfff194fc0
whyhilde <whyhilde@gmail.com>
2026-06-20 13:22:22 +0500

    feat(colors): complete module

--- src/commands/colors.rs
+++ src/commands/colors.rs
@@ -1,8 +1,15 @@
 use anyhow::Result;
+use colored::*;
 
 pub fn run() -> Result<()> {
-    println!("\\e[31m Red");
+    println!("|030| {}    |040| {}", "Black".black(), "Black".on_black());
+    println!("|031| {}      |041| {}", "Red".red(), "Red".on_red());
+    println!("|032| {}    |042| {}", "Green".green(), "Green".on_green());
+    println!("|033| {}   |043| {}", "Yellow".yellow(), "Yellow".on_yellow());
+    println!("|034| {}     |044| {}", "Blue".blue(), "Blue".on_blue());
+    println!("|035| {}  |045| {}", "Magenta".magenta(), "Magenta".on_magenta());
+    println!("|036| {}     |046| {}", "Cyan".cyan(), "Cyan".on_cyan());
+    println!("|037| {}    |047| {}", "White".white(), "White".on_white());
 
     Ok(())
-    
 }
